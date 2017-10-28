e1428bca33750451e3335f03f7952177034b3c36
Vijay <vshukla684@gmail.com>
2017-10-28 16:54:28 +0530

    Add async verticle deployment

--- src/main/groovy/com/impl/VerticleTest.groovy
+++ src/main/groovy/com/impl/VerticleTest.groovy
@@ -7,6 +7,35 @@ class VerticleTest {
 
     public static void main(String[] arg) {
         Vertx vertx = Vertx.vertx()
+        deployVerticleAsync(vertx)
+    }
+
+    static def deployVerticleByInstance(Vertx vertx) {
+        //Deploy Vertex
         vertx.deployVerticle(new FirstVerticle())
+    }
+
+    static def deployVerticleByClassName(Vertx vertx) {
+        //You can also deploy verticle by class name
+        vertx.deployVerticle("com.impl.verticle.FirstVerticle")
+    }
+
+    static def deployVerticleAsync(Vertx vertx) {
+        /* vertx.deployVerticle(new FirstVerticle(), { res ->
+             if (res.succeeded()) {
+                 println("Verticle has been deployed with id:- ${res.result()}")
+             } else {
+                 println("Deployment is failed")
+             }
+         })*/
+
+
+
+        /*vertx.deployVerticle(new FirstVerticle(), new Handler<AsyncResult<String>>() {
+            @Override
+            public void handle(AsyncResult<String> stringAsyncResult) {
+                System.out.println("FirstVerticle deployment complete")
+            }
+        })*/
     }
 }
